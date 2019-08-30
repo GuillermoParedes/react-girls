8ed3e849615402a01ad94ebf5d877f5b0864c8a3
GD_DEV <gdavid.ptorrez@gmail.com>
2019-08-30 15:26:39 -0400

    remove Alert

--- src/container/App.js
+++ src/container/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import { Alert } from "reactstrap";
 import Profile from "../components/Profile";
 
 function App() {
   return (
     <Container>
-      <Row className="p-2">
-        <Col className="bg-success text-center pt-5 pb-5">
+      <Row>
+        <Col xs="4" className="bg-success text-center pt-5 pb-5">
           <Profile
             name="Guillermo Paredes Torrez"
             job="Software Frontend Engineer"
           />
           <hr className="bg-white" />
         </Col>
-        <Col className="bg-dark text-center">
-          <Alert color="primary">This is a primary alert — check it out!</Alert>
-        </Col>
+        <Col xs="8" className="bg-dark text-center"></Col>
       </Row>
     </Container>
   );
