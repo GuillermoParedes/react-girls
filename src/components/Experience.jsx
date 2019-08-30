54103ed32f6131dccabe62694e39a326b70b8c84
GD_DEV <gdavid.ptorrez@gmail.com>
2019-08-30 15:57:47 -0400

    changes orientation text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Experience = ({ title, experiences }) => {
   return (
-    <section>
+    <section className="mb-5 mt-5">
       <h4 className="text-uppercase text-success">{title}</h4>
       <hr className="bg-white" />
       {experiences.map((e, i) => {
         return (
           <div key={i}>
-            <h5 className="text-white">{e.title}</h5>
-            <ol>
+            <h5 className="text-left text-white">{e.title}</h5>
+            <ul className="text-left">
               {e.descriptions.map((d, ii) => {
                 return (
                   <li className="text-success" key={ii}>
@@ -17,7 +17,7 @@ const Experience = ({ title, experiences }) => {
                   </li>
                 );
               })}
-            </ol>
+            </ul>
           </div>
         );
       })}
