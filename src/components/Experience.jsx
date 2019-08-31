83d1da77c8669808b9b86b38fca4dc1ac5c681eb
gd <gdavid.ptorrez@gmail.com>
2019-08-30 22:39:51 -0400

    add button

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const Experience = ({ title, experiences }) => {
   return (
-    <section className="mb-5">
-      <h4 className="text-left text-uppercase text-success">{title}</h4>
+    <section className="mb-5 p-relative ">
+      <h4 className="text-left text-uppercase text-success">
+        {title}
+        <Button className="ml-2" outline color="success">
+          Add
+        </Button>
+      </h4>
       <hr className="bg-dark" />
       {experiences.map((e, i) => {
         return (
